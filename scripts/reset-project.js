e7ca18da997fcddbfab9463ebad4e2a6ad912b27
deepak kumar gurram <deepak.deepak.g280@gmail.com>
2025-05-01 13:48:57 +0530

    eslint errors are added

--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -52,7 +52,7 @@ const moveDirectories = async (userInput) => {
       await fs.promises.mkdir(exampleDirPath, { recursive: true });
       console.log(`📁 /${exampleDir} directory created.`);
     }
-
+    let a;
     // Move old directories to new app-example directory or delete them
     for (const dir of oldDirs) {
       const oldDirPath = path.join(root, dir);
